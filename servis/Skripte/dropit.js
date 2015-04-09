07a11b550d19f11aa5caea5e8dec05991b2f8390
EldarGranulo <apokalipso_t@hotmail.com>
2015-04-09 20:31:35 +0200

    Nove ispravke u validaciji
    Ispravljena jedna greska u html dokumentu i ispravljena validacija

--- servis/Skripte/dropit.js
+++ servis/Skripte/dropit.js
@@ -5,9 +5,17 @@ $(document).ready(function() {
 
     function dropItDown() {
         $('#drpdown').find('ul').css('visibility', 'visible');
-    };
+    }
 
     function pullItBack() {
         $('#drpdown').find('ul').css('visibility', 'hidden');
-    };
+    }
+
+    textArea = document.inputForm.Poruka.value;
+    if(textArea != null) {
+        textArea = textArea.replace(/(^\s*)|(\s*$)/gi, "");
+        textArea = textArea.replace(/[ ]{2,}/gi, " ");
+        textArea = textArea.replace(/\n /, "\n");
+        document.inputForm.Poruka.value = textArea;
+    }
 });
--- servis/Skripte/validate.js
+++ servis/Skripte/validate.js
@@ -1,30 +1,79 @@
 
-function validateInput(){
 
+
+function validateInput(){
+    var isValid = true;
+    var mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    /* clear textarea spaces */
+    textArea = document.inputForm.Poruka.value;
+    textArea = textArea.replace(/(^\s*)|(\s*$)/gi,"");
+    textArea = textArea.replace(/[ ]{2,}/gi," ");
+    textArea = textArea.replace(/\n /,"\n");
+    document.inputForm.Poruka.value = textArea;
     if( document.inputForm.Name.value == "")
     {
-        alert( "Morate unijeti ime!" );
+        document.inputForm.Name.setCustomValidity("Morate unijeti ime!");
         document.inputForm.Name.focus();
-        return false;
+        isValid = false;
     }
-    if(containsNumbers(document.inputForm.Name.value)){
-        alert( "Ime ne smije sadržavati brojeve!" );
+    else if(containsNumbers(document.inputForm.Name.value)){
+        document.inputForm.Name.setCustomValidity( "Ime ne smije sadržavati brojeve!");
         document.inputForm.Name.focus();
-        return false;
+        isValid = false;
     }
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.inputForm.Email.value))
-    {
-        alert( "Unesite email u pravom formatu!" );
+    else{
+        document.inputForm.Name.setCustomValidity("");
+    }
+    if(document.inputForm.Email.value == "" || document.inputForm.Email.value == null){
+        document.inputForm.Email.setCustomValidity("Morate unijeti e - mail!" );
         document.inputForm.Email.focus();
-        return false;
+        isValid = false;
     }
+    else if(!mailRegex.test(document.inputForm.Email.value))
+    {
+        document.inputForm.Email.setCustomValidity("Unesite email u pravom formatu!" );
+        document.inputForm.Email.focus();
+        isValid = false;
+    }
+    else{
+        document.inputForm.Email.setCustomValidity("");
+    }
+
+    if(document.inputForm.Brojtel.value != ""){
+        if(document.inputForm.Brojtel.value.length != 9){
+            document.inputForm.Brojtel.setCustomValidity("Telefon mora imati 9 cifara!");
+            document.inputForm.Brojtel.focus();
+            isValid = false;
+        }
+        else if(!containsNumbers(document.inputForm.Brojtel.value)){
+            document.inputForm.Brojtel.setCustomValidity("Telefon ne smije sadrzavati slova, brojeve i specijalne znakove!");
+            document.inputForm.Brojtel.focus();
+            isValid = false;
+        }
+        else{
+            document.inputForm.Brojtel.setCustomValidity("");
+        }
+    }
+
     if(document.inputForm.Poruka.value == ""){
-        alert( "Morate unijeti tekst poruke!" );
-        document.inputForm.Name.focus();
-        return false;
+        document.inputForm.Poruka.setCustomValidity("Morate unijeti tekst poruke!");
+        document.inputForm.Poruka.focus();
+        isValid = false;
+    }
+    else{
+        document.inputForm.Poruka.setCustomValidity("");
     }
 
+    if(document.inputForm.Tip.value == ""){
+        document.inputForm.Poruka.setCustomValidity("Morate izabrati tip!");
+        document.inputForm.Poruka.focus();
+        isValid = false;
+    }
+    else{
+        document.inputForm.Poruka.setCustomValidity("");
+    }
 
+    return isValid;
 }
 
 function containsNumbers(string){
@@ -41,4 +90,11 @@ function setToValueFromCmbBox(){
     var valueCmb = cmbBox.options[cmbBox.selectedIndex].text;
     if(valueCmb == "Ponuda" || valueCmb == "Kupovina") tbToSet.value = "Pravno lice";
     else tbToSet.value = "Pravno/Fizičko lice";
+}
+
+function clearCustomValidity(){
+    var elements = document.inputForm.elements;
+    for(var i = 0; i < elements.length; i++){
+        elements[i].setCustomValidity("");
+    }
 }
